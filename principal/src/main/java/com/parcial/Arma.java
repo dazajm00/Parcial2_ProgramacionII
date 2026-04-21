d2be606844412407de87854e474dc807f658c636
Camila Rodriguez <camilarodriguez@estuidante.uniajc.edu.co>
2026-04-21 15:51:06 -0500

    Implementar clase Arma

---
+++ principal/src/main/java/com/parcial/Arma.java
@@ -0,0 +1,27 @@
+package com.parcial;
+
+public class Arma {
+
+    private final String nombre;
+    private final int    dañoAdicional;
+
+    public Arma(String nombre, int dañoAdicional) {
+        this.nombre        = nombre;
+        this.dañoAdicional = dañoAdicional;
+    }
+
+    public void atacarConArma(Criatura objetivo) {
+        System.out.println(nombre + " golpea a " + objetivo.getNombre()
+                + " causando " + dañoAdicional + " puntos de daño extra.");
+        objetivo.defender(dañoAdicional);
+    }
+
+    /** Retorna el daño adicional que proporciona el arma. */
+    public int getDañoAdicional() {
+        return dañoAdicional;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
